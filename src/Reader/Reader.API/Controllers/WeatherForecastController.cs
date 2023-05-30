57098ff68ba90f1ec9c60d4e149240896236b313
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2023-05-30 20:09:12 +0200

    update

---
+++ src/Reader/Reader.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Reader.API.Controllers
+{
+    [ApiController]
+    [Route("events")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public WeatherForecastController()
+        {
+            
+        }
+    }
+}
