81d85b747faf6b30c8d38fb716d3f5bfb1f7199a
Tomáš Jurásek <tomas.jurasek@alza.cz>
2023-09-23 21:30:55 +0200

    tests

---
+++ src/EventPlanning.Tests/DomainTests/EventAggregateTests.cs
@@ -0,0 +1,12 @@
+﻿namespace EventPlanning.Tests.DomainTests
+{
+    public class EventAggregateTests
+    {
+
+        [Fact]
+        public void Create_When_CorrectData_Should_Success()
+        {
+
+        }
+    }
+}
---
+++ src/EventPlanning.Tests/DomainTests/RegistrationAggregateTests.cs
@@ -0,0 +1,45 @@
+﻿using EventPlanning.Domain.Registration;
+using FluentAssertions;
+
+namespace EventPlanning.Tests.DomainTests
+{
+    public class RegistrationAggregateTests
+    {
+        private readonly RegistrationAggregateFactory _registrationAggregateFactory;
+
+        public RegistrationAggregateTests()
+        {
+            _registrationAggregateFactory = new RegistrationAggregateFactory();
+        }
+
+        [Fact]
+        public void Create_When_CorrectData_Should_Success()
+        {
+            var registration = _registrationAggregateFactory.Create(Id, Attendee);
+
+            registration.IsSuccess.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Create_When_InvalidId_Should_Fail(string id)
+        {
+            var registration = _registrationAggregateFactory.Create(id, Attendee);
+
+            registration.IsFailed.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Create_When_InvalidAttendee_Should_Fail()
+        {
+            var registration = _registrationAggregateFactory.Create(Id, null);
+
+            registration.IsFailed.Should().BeTrue();
+        }
+
+
+        public static string Id => Guid.NewGuid().ToString();
+        public static Attendee Attendee => new Attendee();
+    }
+}
