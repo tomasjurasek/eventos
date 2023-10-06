0edca68fc60912abb86d5426ecc6b1be25d65650
Tomáš Jurásek <tomas.jurasek@alza.cz>
2023-10-06 21:15:13 +0200

    projects

---
+++ src/EventPlanning.ReadModel/EventPlanning.ReadModel.API/Controllers/EventsController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventPlanning.ReadModel.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventsController : ControllerBase
+    {
+        private readonly ILogger<EventsController> _logger;
+
+        public EventsController(ILogger<EventsController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
