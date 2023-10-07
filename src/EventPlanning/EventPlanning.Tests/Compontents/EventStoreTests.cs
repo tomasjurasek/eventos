3dbf175bea1a0a5d858f369892f6418977b1f72e
Tomáš Jurásek <tomas.jurasek@alza.cz>
2023-10-07 21:41:57 +0200

    testcontainers

---
+++ src/EventPlanning/EventPlanning.Tests/Compontents/EventStoreTests.cs
@@ -0,0 +1,30 @@
+﻿using Testcontainers.EventStoreDb;
+
+namespace EventPlanning.Tests.Compontents
+{
+    public class EventStoreTests
+    {
+        private readonly EventStoreDbContainer _eventStoreDbContainer;
+        private readonly Infrastructure.Stores.EventStore _eventStore;
+        public EventStoreTests()
+        {
+
+
+            //_eventStore = new Infrastructure.Stores.EventStore(Options.Create(new Infrastructure.Options.EventStoreOptions
+            //{
+            //    ConnectionString = ""
+            //}));
+        }
+
+        [Fact]
+        public async Task Test()
+        {
+            var eventStoreDbContainer = new EventStoreDbBuilder()
+                .WithImage("eventstore/eventstore:21.2.0-buster-slim")
+                .WithName("aaa").Build();
+
+
+            await eventStoreDbContainer.StartAsync();
+        }
+    }
+}
