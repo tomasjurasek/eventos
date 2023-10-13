ccebfaa4d309be29521f776489b9d65a98757a87
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2023-10-13 22:22:14 +0200

    aggregate root repo

---
+++ src/EventPlanning/EventPlanning.Domain/Common/IAggregateRootRepository.cs
@@ -0,0 +1,10 @@
+﻿using FluentResults;
+
+namespace EventPlanning.Domain.Common
+{
+    public interface IAggregateRootRepository<TAggregate> where TAggregate: IAggregateRoot
+    {
+        Task<Result<TAggregate>> FindAsync(Guid Id);
+        Task<Result> StoreAsync(TAggregate aggregate);
+    }
+}
---
+++ src/EventPlanning/EventPlanning.Infrastructure/Repositories/AggregateRootRepository.cs
@@ -0,0 +1,18 @@
+﻿using EventPlanning.Domain.Common;
+using FluentResults;
+
+namespace EventPlanning.Infrastructure.Repositories
+{
+    internal class AggregateRootRepository<TAggregate> : IAggregateRootRepository<TAggregate> where TAggregate : IAggregateRoot
+    {
+        public Task<Result<TAggregate>> FindAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> StoreAsync(TAggregate aggregate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
