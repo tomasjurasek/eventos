5528fbff666f6a8c9b5ad0832cdfb37a11d5d702
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2023-11-20 14:24:03 +0100

    fix build

--- src/EventPlanning/EventPlanning.API/Program.cs
+++ src/EventPlanning/EventPlanning.API/Program.cs
@@ -1,5 +1,4 @@
 using EventPlanning.Application.Extensions;
-using EventPlanning.Domain.Extensions;
 using EventPlanning.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,12 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplicationServices();
-builder.Services.AddDomainServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
