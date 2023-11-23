44bac6442853bc9bcf3764456ee6b44108e8dac0
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2023-11-23 22:28:03 +0100

    update

--- src/EventPlanning/EventPlanning.Application/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ src/EventPlanning/EventPlanning.Application/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -17,21 +17,25 @@ namespace EventPlanning.Application.Commands.CreateEvent
         {
             var result = EventAggregate.Create(Guid.NewGuid(), command.Name, command.Description, command.Organizer, command.Address, command.Capacity, command.StartedAt, command.FinishedAt);
 
-            if (result.IsSuccess)
+            return result switch
+            {
+                { IsSuccess: true } => await StoreAsync(result),
+                _ => result.ToResult()
+            };
+
+            async Task<Result<CommandResult>> StoreAsync(Result<EventAggregate> result)
             {
                 var storeResult = await _eventRepository.StoreAsync(result.Value);
-                if (storeResult.IsSuccess)
+
+                return storeResult switch
                 {
-                    return Result.Ok(new CommandResult
+                    { IsSuccess: true } => Result.Ok(new CommandResult
                     {
                         Id = result.Value.Id
-                    });
-                }
-
-                return storeResult;
-
+                    }),
+                    _ => storeResult
+                };
             }
-            return result.ToResult();
         }
     }
 }
